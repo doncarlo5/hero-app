435443d48786f3a837f32da31b937f92d7ae7bdc
doncarlo5 <pro.julien.thomas@gmail.com>
2025-07-02 23:36:42 +0200

    remove classname

--- apps/mobile/components/ui/button.tsx
+++ apps/mobile/components/ui/button.tsx
@@ -1,11 +1,11 @@
+import { TextClassContext } from "@/components/ui/text";
+import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 import { Pressable } from "react-native";
-import { cn } from "@/lib/utils";
-import { TextClassContext } from "@/components/ui/text";
 
 const buttonVariants = cva(
-	"group flex items-center justify-center rounded-md web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2",
+	"group flex items-center justify-center web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2",
 	{
 		variants: {
 			variant: {
@@ -19,10 +19,10 @@ const buttonVariants = cva(
 				link: "web:underline-offset-4 web:hover:underline web:focus:underline",
 			},
 			size: {
-				default: "h-10 px-4 py-2 native:h-12 native:px-5 native:py-3",
-				sm: "h-9 rounded-md px-3",
-				lg: "h-11 rounded-md px-8 native:h-14",
-				icon: "h-10 w-10",
+				default: "h-10 px-4 py-2 native:h-12 native:px-5 native:py-3 ",
+				sm: "h-9  px-3",
+				lg: "h-11  px-8 native:h-14",
+				icon: "h-10 w-10 ",
 			},
 		},
 		defaultVariants: {
