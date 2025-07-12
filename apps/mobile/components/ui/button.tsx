59d788184ec734a1fcacee1abe174f20e93c7189
doncarlo5 <pro.julien.thomas@gmail.com>
2025-07-12 10:38:51 +0200

    improve styling button

--- apps/mobile/components/ui/button.tsx
+++ apps/mobile/components/ui/button.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { Pressable } from "react-native";
 
 const buttonVariants = cva(
-	"group flex items-center justify-center web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2",
+	"group flex items-center rounded-full justify-center web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2",
 	{
 		variants: {
 			variant: {
@@ -19,10 +19,10 @@ const buttonVariants = cva(
 				link: "web:underline-offset-4 web:hover:underline web:focus:underline",
 			},
 			size: {
-				default: "h-10 px-4 py-2 native:h-12 native:px-5 native:py-3 ",
-				sm: "h-9  px-3",
-				lg: "h-11  px-8 native:h-14",
-				icon: "h-10 w-10 ",
+				default: "h-10 px-4 py-2 native:h-12 native:px-5 native:py-3",
+				sm: "h-9 px-3",
+				lg: "h-11 px-8 native:h-14",
+				icon: "h-10 w-10",
 			},
 		},
 		defaultVariants: {
