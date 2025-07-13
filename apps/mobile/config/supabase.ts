ab09192ef51a930a06d2b4cf8a8997bf4eb407f7
doncarlo5 <pro.julien.thomas@gmail.com>
2025-07-13 23:04:44 +0200

    imports

--- apps/mobile/config/supabase.ts
+++ apps/mobile/config/supabase.ts
@@ -1,9 +1,9 @@
 import { AppState } from "react-native";
 
-import "react-native-get-random-values";
-import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient, processLock } from "@supabase/supabase-js";
+import "react-native-get-random-values";
+import "react-native-url-polyfill/auto";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
