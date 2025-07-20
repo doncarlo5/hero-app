87ffaeda445b73e86bde9b092b05e4c13192aa19
doncarlo5 <pro.julien.thomas@gmail.com>
2025-07-20 15:55:31 +0200

    add user type

---
+++ apps/mobile/types/user.type.ts
@@ -0,0 +1,10 @@
+export type UserType = {
+	_id: string;
+	firstName: string;
+	lastName: string;
+	email: string;
+	hasSeenOnboarding: boolean;
+	createdAt: Date;
+	updatedAt: Date;
+	__v: number;
+};
