376cb0e5231c0d96e1bd70005d6fd17fed02ccd2
doncarlo5 <pro.julien.thomas@gmail.com>
2025-07-20 15:55:05 +0200

    handle feedbackModalVisible in settings

--- apps/mobile/app/(protected)/(tabs)/settings.tsx
+++ apps/mobile/app/(protected)/(tabs)/settings.tsx
@@ -9,15 +9,18 @@ import {
 	Trophy,
 	UserCog,
 } from "lucide-react-native";
+import { useState } from "react";
 import { Alert, Pressable, View } from "react-native";
 
 import { Text } from "@/components/ui/text";
 import { useAuth } from "@/context/supabase-provider";
 import { useRouter } from "expo-router";
+import FeedbackModal from "../feedback-modal";
 
 export default function Settings() {
 	const { signOut } = useAuth();
 	const router = useRouter();
+	const [feedbackModalVisible, setFeedbackModalVisible] = useState(false);
 
 	return (
 		<View className="flex-1 bg-background dark:bg-background-dark p-4 flex flex-col gap-4 justify-center">
@@ -91,7 +94,10 @@ export default function Settings() {
 				</View>
 
 				<View className="w-1/2 pr-1 pb-2">
-					<Pressable className="h-32 rounded-md border flex items-center justify-center border-border dark:border-border-dark bg-background dark:bg-background-dark">
+					<Pressable
+						onPress={() => setFeedbackModalVisible(true)}
+						className="h-32 rounded-md border flex items-center justify-center border-border dark:border-border-dark bg-background dark:bg-background-dark"
+					>
 						<View className="flex flex-col items-center gap-1">
 							<Star size={32} strokeWidth={1.1} />
 							<Text>Feedback</Text>
@@ -113,6 +119,11 @@ export default function Settings() {
 					</Pressable>
 				</View>
 			</View>
+
+			<FeedbackModal
+				visible={feedbackModalVisible}
+				onClose={() => setFeedbackModalVisible(false)}
+			/>
 		</View>
 	);
 }
