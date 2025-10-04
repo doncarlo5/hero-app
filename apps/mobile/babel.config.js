8e8a7782702c09c608281d167571fc3e9189ddbc
doncarlo5 <pro.julien.thomas@gmail.com>
2025-10-04 19:03:22 +0200

    react-native-worklets

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,14 +2,7 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: [
-			[
-				"babel-preset-expo",
-				{
-					jsxImportSource: "nativewind",
-					// Disable worklets plugin to avoid conflicts
-					unstable_disableES6Transforms: false,
-				},
-			],
+			["babel-preset-expo", { jsxImportSource: "nativewind" }],
 			"nativewind/babel",
 		],
 		plugins: [
