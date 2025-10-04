945547bdd2bb51a3db3306d23c57a2004ceee076
doncarlo5 <pro.julien.thomas@gmail.com>
2025-10-04 18:57:19 +0200

    unstable_disableES6Transforms

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,7 +2,14 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: [
-			["babel-preset-expo", { jsxImportSource: "nativewind" }],
+			[
+				"babel-preset-expo",
+				{
+					jsxImportSource: "nativewind",
+					// Disable worklets plugin to avoid conflicts
+					unstable_disableES6Transforms: false,
+				},
+			],
 			"nativewind/babel",
 		],
 		plugins: [
